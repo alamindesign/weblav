e489e0393be3881e500f714453ccf9878df4a414
alamindesign <mail.muhammadit@gmail.com>
2023-01-22 09:21:23 +0600

    new insert

--- webLavProject/studentApp/urls.py
+++ webLavProject/studentApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='student' ),
+    path('', views.studenthome, name='student' ),
     path('del/<int:id>', views.remove, name='remove'),
+    path('addnewstudent', views.addnewstudent , name='addnewstudent'),
+    path('save', views.savestudent, name="saveStudent")
 ]
--- webLavProject/studentApp/views.py
+++ webLavProject/studentApp/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render,redirect
 from . import models
+from . import forms
 # Create your views here.
-def home(request):
+def studenthome(request):
     all_student = models.Students.objects.all()
     all_programs = models.Program.objects.all()
     return render(request,'studentApp\index.html',{'Students':all_student,'Programs': all_programs})
 def remove(request,id):
     student = models.Students.objects.get(id = id)
     student.delete()
-    return redirect(home)
+    return redirect(studenthome)
+def addnewstudent(request):
+    stf = forms.StudentForm()
+    return render(request,'studentApp/addnewstudent.html',{'stf':stf})
+def savestudent(request):
+    if request.method == "POST":
+        sform = forms.StudentForm(request.POST)
+        if sform.is_valid():
+            try:
+                sform.save
+            except:
+                pass
+            return redirect(studenthome)
